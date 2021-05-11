d7ff438ace7c9877321aa18e77e3403f73fd9afb
Sahithi Pisupati <sp3429@drexel.edu>
2021-05-10 23:35:31 -0400

    class based component

---
+++ Header.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+return(
+<h1></h1>
+)
+export default Header
---
+++ MemeGenerator.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM, { render } from "react-dom"
+
+class MemeGenerator extends React.Component {
+
+}
+
+render(){
+    return(
+        <h1></h1>
+    )
+}
+
+export default MemeGenerator
