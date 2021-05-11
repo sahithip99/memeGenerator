816739c8136deee7b7cb758eb725bf10d9475816
Sahithi Pisupati <sp3429@drexel.edu>
2021-05-10 23:19:31 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Create the boilerplate to get React to render something on the screen
+ * Render an <App /> component, which you'll need to create separately
+ */
+import React from "react"
+import App from "App"
+
+return(
+  <App />
+)
