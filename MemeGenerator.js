3d5ab30b44d8f44b872639302af17f7c2d068fad
Sahithi Pisupati <sp3429@drexel.edu>
2021-05-10 23:53:14 -0400

    fetch API call

--- MemeGenerator.js
+++ MemeGenerator.js
@@ -5,9 +5,20 @@ class MemeGenerator extends React.Component {
     constructor() {
         super()
         this.state={
-    
+            topText: "",
+            bottomText: "",
+            imageURL:"http://i.imgflip.com/1bij.jpg",
+            memesImages: []
         }
     }
+
+    componentDidMount(){
+        fetch()
+            .then(response => response.json())
+            .then(data => {const memes = data
+                          this.setState({memesImages: memes})})
+    }
+
     render(){
         return(
             <h1>MEME GENERATOR</h1>
